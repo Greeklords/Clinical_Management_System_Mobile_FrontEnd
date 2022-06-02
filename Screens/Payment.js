fee2f10bee17da85ecb4f72c8bc8688e6ac79c60
Maheshi <dissanayakedmmk.19@uom.lk>
2022-06-02 22:24:49 +0530

    First Project

--- Screens/Payment.js
+++ Screens/Payment.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity,TextInput } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native'; 
 
@@ -8,8 +8,18 @@ const Payment = () => {
   
     return (
       <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <TouchableOpacity onPress={()=>navigation.navigate('Home')}>
-            <Text>Payment</Text>
+
+            //<TouchableOpacity onPress={()=>navigation.navigate('Home')}>
+
+            <Text >Specialization</Text>
+          <TextInput
+
+
+           
+            onChangeText={(text) => { this.setState({ Specialization: text }) }}
+            style={{ borderWidth: 2, borderColor: 'skyblue', margin: 20, textAlign: 'center' }}
+          />
+          
         </TouchableOpacity> 
   
       </View> 
